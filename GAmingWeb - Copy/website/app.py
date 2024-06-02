63336f8bb93c8906cf5e7933d6b14c88b7cb07a5
Dhananjaya <144225924+DHANANJAYA0013@users.noreply.github.com>
2024-06-02 12:06:22 +0530

    Update app.py

--- GAmingWeb - Copy/website/app.py
+++ GAmingWeb - Copy/website/app.py
@@ -483,4 +483,4 @@ def format_datetime(value, format='%Y-%m-%d %H:%M:%S'):
     return local_dt.strftime(format)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
